0cbaac49fce40f130fd640d97ff1933a1128353e
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-06-23 17:06:47 +0300

    λογικά αυτό δεν θα δούλευε

--- delete all tables.sql
+++ delete all tables.sql
@@ -1,11 +1,1 @@
-DROP TABLE address;
-DROP TABLE accident;
-DROP TABLE contracts;
-DROP TABLE customer;
-DROP TABLE driver;
-DROP TABLE driver_and_vehicle_in_accident;
-DROP TABLE driver_contract;
-DROP TABLE driver_vehicle;
-DROP TABLE phone_numbers;
-DROP TABLE vehicles;
-DROP TABLE model;
+DROP TABLE address, accident, contracts, customer, driver, driver_and_vehicle_in_accident, driver_contract, driver_vehicle, phone_numbers, vehicles, model;
