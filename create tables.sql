9d9caa59f98d091c8942dd81d9b0d9dc90ca571e
NickGeo1 <60719667+NickGeo1@users.noreply.github.com>
2021-06-23 16:46:17 +0300

    added sql file

---
+++ create tables.sql
@@ -0,0 +1,118 @@
+CREATE TABLE CONTRACTS 
+(
+    number INT NOT NULL,
+    category VARCHAR(20) NOT NULL,
+    starting_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    price INT NOT NULL,
+    CUSTOMER_email VARCHAR(45) NOT NULL,
+    VEHICLES_plates VARCHAR(10) NOT NULL,
+    
+    primary key(number,CUSTOMER_email,VEHICLES_plates),
+	unique(CUSTOMER_email),
+	unique(VEHICLES_plates),
+	unique(number)
+);
+
+CREATE TABLE CUSTOMER
+(
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    gender VARCHAR(6) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    license_number BIGINT NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    CONTRACTS_number INT NOT NULL,
+    primary key(license_number,email,CONTRACTS_number),
+	unique(license_number),
+	unique(email),
+	unique(CONTRACTS_number)
+);
+
+CREATE TABLE VEHICLES
+(
+    plates VARCHAR(10) NOT NULL,
+    registry_number INT NOT NULL,
+    color VARCHAR(10) NOT NULL,
+    year INT NOT NULL,
+    current_price INT NOT NULL,
+    contract_number INT NOT NULL,
+    caregory VARCHAR(20) NOT NULL,
+    CUSTOMER_email VARCHAR(45) NOT NULL,
+    MODEL_model VARCHAR(20) NOT NULL,
+    PRIMARY KEY (plates,registry_number,contract_number,CUSTOMER_email),
+    unique(plates),
+    unique(contract_number),
+    unique(registry_number),
+	unique(CUSTOMER_email)
+);
+
+CREATE TABLE DRIVER
+(
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    gender VARCHAR(6) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    license_number BIGINT NOT NULL,
+    primary key(license_number),
+);
+
+CREATE TABLE DRIVER_VEHICLE
+(
+    DRIVER_license_number BIGINT NOT NULL,
+    VEHICLES_plates VARCHAR(10) NOT NULL,
+    primary key(DRIVER_license_number,VEHICLES_plates)
+);
+
+CREATE TABLE DRIVER_CONTRACT
+(
+    DRIVER_license_number BIGINT NOT NULL,
+    CONTRACTS_number INT NOT NULL,
+    primary key(DRIVER_license_number,CONTRACTS_number)
+);
+
+CREATE TABLE ACCIDENT
+(
+    id INT NOT NULL,
+    date DATE NOT NULL,
+    time TIME NOT NULL,
+    description VARCHAR(50) NOT NULL,
+    primary key(id)
+);
+
+CREATE TABLE DRIVER_AND_VEHICLE_IN_ACCIDENT
+(
+    ACCIDENT_id INT NOT NULL,
+    VEHICLES_plates VARCHAR(10) NOT NULL,
+    DRIVER_license_number BIGINT NOT NULL,
+    primary key(ACCIDENT_id,VEHICLES_plates,DRIVER_license_number),
+    unique(ACCIDENT_id,VEHICLES_plates),
+    unique(ACCIDENT_id,DRIVER_license_number)
+);
+
+CREATE TABLE ADDRESS
+(
+    zip_code INT NOT NULL,
+    city VARCHAR(30) NOT NULL,
+    country VARCHAR(30) NOT NULL,
+    street VARCHAR(45) NOT NULL,
+    number INT NOT NULL,
+    license BIGINT NOT NULL,
+    type VARCHAR(8) NOT NULL,
+    primary key(license)
+);
+
+CREATE TABLE PHONE_NUMBERS
+(
+    number BIGINT NOT NULL,
+    CUSTOMER_email VARCHAR(45) NOT NULL,
+    primary key(number,CUSTOMER_email)    
+);
+
+CREATE TABLE MODEL
+(
+    model VARCHAR(20) NOT NULL,
+    type VARCHAR(15) NOT NULL,
+    manufacturer VARCHAR(20) NOT NULL,
+    primary key(model)
+);
