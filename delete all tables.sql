bd160cd864045a60e0ddd529a4a4f10618870bc7
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-06-23 16:56:38 +0300

    Σε περίπτωση που κάνετε μαλακία, ορίστε το σκριπτάκι

---
+++ delete all tables.sql
@@ -0,0 +1,11 @@
+DROP TABLE address;
+DROP TABLE accident;
+DROP TABLE contracts;
+DROP TABLE customer;
+DROP TABLE driver;
+DROP TABLE driver_and_vehicle_in_accident;
+DROP TABLE driver_contract;
+DROP TABLE driver_vehicle;
+DROP TABLE phone_numbers;
+DROP TABLE vehicles;
+DROP TABLE model;
