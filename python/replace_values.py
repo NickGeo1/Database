e7d4930748abc1a5b5cfdd1b2dd6c626cbf2116e
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-06-24 19:48:29 +0300

    made driver contracts

--- python/replace_values.py
+++ python/replace_values.py
@@ -5,26 +5,27 @@ lines = f.read()
 lines = lines.split('\n')
 f.close()
 
-f = io.open('VEHICLES.sql', 'r', encoding='UTF-8')
+f = io.open('DRIVER_CONTRACT.sql', 'r', encoding='UTF-8')
 lines_2 = f.read()
 lines_2 = lines_2.split('\n')
 f.close()
 
-new_file = io.open('VEHICLES2.sql', 'a', encoding='UTF-8')
+new_file = io.open('DRIVER_CONTRACT2.sql', 'a', encoding='UTF-8')
 
 for i in range(0, 500):
     previous_query  = lines[i]
+    previous_query  = previous_query.replace(',', '')
     previous_query  = previous_query.split()
-    previous_query  = "'" + previous_query[-3][1:] + " - " + previous_query[-1][:-3] + "'"
-    plate           = previous_query
+    contract_no     = previous_query[11][1:]
     query = lines_2[i]
-    query = query.replace("'platespk'", plate)
+    query = query.replace("'contracts'", contract_no)
     print(query)
     new_file.write(query + '\n')
 
 new_file.close()
 
 query = "insert into CONTRACTS (number, category, starting_date, end_date, price, CUSTOMER_email, VEHICLES_plates) values (3922661562, 'Simple', '1991-07-29', '1985-05-10', 19806, 'mtupper0@twitter.com', 'PME - 7929');"
+
+query = query.replace(',', '')
 query = query.split()
-query = "'" + query[-3][1:] + " - " + query[-1][:-3] + "'"
-print(query)
+print(query[11][1:])
