9dbf7f3b045306b5800214937760ec8caab56eb9
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-06-24 19:10:11 +0300

    έγιναν τα e-mail

--- python/replace_values.py
+++ python/replace_values.py
@@ -14,15 +14,17 @@ new_file = io.open('VEHICLES2.sql', 'a', encoding='UTF-8')
 
 for i in range(0, 500):
     previous_query  = lines[i]
+    previous_query  = previous_query.replace(',', '')
     previous_query  = previous_query.split()
-    contract         = previous_query[11][1:-1]
+    email           = previous_query[-4]
     query = lines_2[i]
-    query = query.replace("'contract'", contract)
+    query = query.replace("'email'", email)
     print(query)
     new_file.write(query + '\n')
 
 new_file.close()
 
 '''query = "insert into CONTRACTS (number, category, starting_date, end_date, price, CUSTOMER_email, VEHICLES_plates) values (3922661562, 'Simple', '1991-07-29', '1985-05-10', 19806, 'mtupper0@twitter.com', 'PME - 7929');"
+query = query.replace(',', '')
 query = query.split()
-print(query[11][1:-1])'''
+print(query[-4])'''
