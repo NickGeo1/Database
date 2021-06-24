8c18ac6891eee9c3b184af8f26501f4b9d866213
GeorgeMC2610 <giorgosseimenis@gmail.com>
2021-06-24 19:04:37 +0300

    έγιναν τα contracts

--- python/replace_values.py
+++ python/replace_values.py
@@ -1,29 +1,28 @@
 import io
 
-f = open('CUSTOMER.sql', 'r')
+f = open('CONTRACTS.sql', 'r')
 lines = f.read()
 lines = lines.split('\n')
 f.close()
 
-f = io.open('ADDRESS.sql', 'r', encoding='UTF-8')
+f = io.open('VEHICLES.sql', 'r', encoding='UTF-8')
 lines_2 = f.read()
 lines_2 = lines_2.split('\n')
 f.close()
 
-new_file = io.open('ADDRESS2.sql', 'a', encoding='UTF-8')
+new_file = io.open('VEHICLES2.sql', 'a', encoding='UTF-8')
 
 for i in range(0, 500):
-    previous_query  = lines[i].replace(',', '')
+    previous_query  = lines[i]
     previous_query  = previous_query.split()
-    license         = previous_query[-3]
+    contract         = previous_query[11][1:-1]
     query = lines_2[i]
-    query = query.replace("'license'", license)
+    query = query.replace("'contract'", contract)
     print(query)
     new_file.write(query + '\n')
 
 new_file.close()
 
-'''query = "insert into CUSTOMER (firstname, lastname, gender, date_of_birth, license_number, email, CONTRACTS_number) values ('Mychal', 'Eslinger', 'Male', '24/7/1960', 9141099782, 'mtupper0@twitter.com', 3922661562);"
-query = query.replace(',', '')
+'''query = "insert into CONTRACTS (number, category, starting_date, end_date, price, CUSTOMER_email, VEHICLES_plates) values (3922661562, 'Simple', '1991-07-29', '1985-05-10', 19806, 'mtupper0@twitter.com', 'PME - 7929');"
 query = query.split()
-print(query[-3])'''
+print(query[11][1:-1])'''
