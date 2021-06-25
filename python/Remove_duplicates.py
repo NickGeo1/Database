e75f848876ea64aa97cab571a0d8c9f1fd48ff09
NickGeo1 <60719667+NickGeo1@users.noreply.github.com>
2021-06-25 03:02:24 +0300

    improved py script. Now it is working for all the tables

---
+++ python/Remove_duplicates.py
@@ -0,0 +1,42 @@
+import io
+
+k = int(input("Give the column number (starting from 1) where the primary key is"))
+filename = input("Give the file name without the sql extension")
+
+file = io.open(filename+".sql", 'r', encoding='UTF-8')
+lines = file.read()
+
+lines = lines.split('(')
+file.seek(0)
+wholelines = file.readlines()
+
+primary_keys=[]
+lineindex=0
+
+for i in range (2,len(lines),2):
+
+    lineindex = lineindex+1 if i>2 else lineindex
+    
+    if(i!=len(lines)-1):
+        lines[i] = lines[i][0:-16-len(filename)]
+    else:
+        lines[i] = lines[i][0:-2]
+
+    temp=lines[i].split(",")[k-1]
+
+    if(temp in primary_keys):
+        wholelines[lineindex] = ''       
+    else:
+        primary_keys.append(temp)
+
+print("Unique primary keys: "+str(len(primary_keys)) + " Duplicates: "+ str(len(wholelines)-len(primary_keys)))
+
+file.close()
+
+newfile = io.open("UNIQUE_"+filename+".sql", 'w+', encoding='UTF-8')
+for l in wholelines:
+    newfile.write(l)
+
+newfile.close()
+
+print("Exported unique primary keys successfuly")
