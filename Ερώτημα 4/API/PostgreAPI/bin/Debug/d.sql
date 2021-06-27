96f4839a5c613f6681162d7533adc127ea58bb96
NickGeo1 <60719667+NickGeo1@users.noreply.github.com>
2021-06-28 01:11:25 +0300

    κιαλη διορθωση γτ πιαχτηκαν μαλακιες στα copy paste

--- Ερώτημα 4/API/PostgreAPI/bin/Debug/d.sql
+++ Ερώτημα 4/API/PostgreAPI/bin/Debug/d.sql
@@ -1,6 +1,7 @@
 -- d. Ποια κατηγορία ασφάλισης παρουσιάζει βάσει των συμβολαίων τον μεγαλύτερο τζίρο
 -- (2 παραλλαγές: σε απόλυτους αριθμούς, με αναγωγή βάσει πλήθους συμβολαίων).
 
+SELECT category , sum(price) AS total_profit , COUNT(number) AS total_contracts
 FROM CONTRACTS  
 group by category
 order by sum(price) desc
