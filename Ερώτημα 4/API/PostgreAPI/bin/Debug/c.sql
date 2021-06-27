50d41d821d7bb22e4ba2eadb0357f12b6c8b5f2c
NickGeo1 <60719667+NickGeo1@users.noreply.github.com>
2021-06-27 22:16:38 +0300

    Update c.sql

--- Ερώτημα 4/API/PostgreAPI/bin/Debug/c.sql
+++ Ερώτημα 4/API/PostgreAPI/bin/Debug/c.sql
@@ -1,8 +1,6 @@
 -- c. Ποιος είναι ο αριθμός των συμβολαίων που υπεγράφησαν (παραλλαγή: που δεν
 -- ανανεώθηκαν) ανά ασφαλιστική κατηγορία και ανά έτος για την πενταετία 2016-2020.
 
---αν θέλω να δείξω το year συγκεκριμένα , στο select αντί για starting_date-> extract(year FROM starting_date)
-
 SELECT starting_date, category, COUNT(number)
 FROM CONTRACTS
 WHERE extract( year FROM starting_date ) BETWEEN 2016 AND 2020
